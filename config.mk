383008b59b8350c839001eb44b429e3d9151273c
elysia <3302843873@qq.com>
2026-04-13 14:08:58 +0800

    fix

--- config.mk
+++ config.mk
@@ -15,15 +15,15 @@ XINERAMALIBS ?= $(shell ${PKG_CONFIG} --libs xinerama)
 XINERAMAFLAGS ?= -DXINERAMA
 
 # freetype
-FREETYPELIBS ?= $(shell ${PKG_CONFIG} --libs fontconfig xft freetype2)
-FREETYPEINC ?= $(shell ${PKG_CONFIG} --cflags fontconfig xft freetype2)
+FREETYPELIBS ?= $(shell ${PKG_CONFIG} --libs fontconfig xft freetype2 imlib2 xrender xext)
+FREETYPEINC ?= $(shell ${PKG_CONFIG} --cflags fontconfig xft freetype2 imlib2 xrender xext)
 # OpenBSD (uncomment)
 #FREETYPEINC = ${X11INC}/freetype2
 #MANPREFIX = ${PREFIX}/man
 
 # includes and libs
-INCS ?= $(shell ${PKG_CONFIG} --cflags x11 xinerama xft fontconfig freetype2)
-LIBS ?= $(shell ${PKG_CONFIG} --libs --static x11 xinerama xft fontconfig freetype2) -lXrender -lImlib2 -lXext -lintl -liconv
+INCS ?= $(shell ${PKG_CONFIG} --cflags x11 xinerama xft fontconfig freetype2 imlib2 xrender xext)
+LIBS ?= $(shell ${PKG_CONFIG} --libs --static x11 xinerama xft fontconfig freetype2 imlib2 xrender xext) -lintl -liconv
 
 # flags
 CPPFLAGS += -D_DEFAULT_SOURCE -D_BSD_SOURCE -D_XOPEN_SOURCE=700L -DVERSION=\"${VERSION}\" ${XINERAMAFLAGS}
